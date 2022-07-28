476b199eeb89ae55e77539f93ee31aa7943e80d8
tahseensikder <tahseensikder@gmail.com>
2022-07-28 13:43:18 +0600

    banner_non_responsive

--- js/app.js
+++ js/app.js
@@ -5,4 +5,12 @@ $(function(){
     logo.toggleClass("activeLogo")
     navMenu.toggleClass("activeMenu")
    })
+   $("#banner .banner_slider").slick({
+      prevArrow:'.left_arrow',
+      nextArrow:'.right_arrow',
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      speed: 1000,
+   })
 })
