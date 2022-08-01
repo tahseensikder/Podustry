d6377ba75ed3575ef914f301b9071082c2c654c9
tahseensikder <tahseensikder@gmail.com>
2022-08-01 10:40:16 +0600

    blog_responsive 1.0.0

--- js/app.js
+++ js/app.js
@@ -59,8 +59,43 @@ $(function(){
     slidesToScroll: 1,
     prevArrow:'.blogLeft',
     nextArrow:'.blogRight',
-    centerMode: true,
     centerPadding: "0px",
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+           settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+         // You can unslick at a given breakpoint now by adding:
+         // settings: "unslick"
+         // instead of a settings object
+    ]
   })
 // Counter Up 
   $('#num1').countMe(7,5);
