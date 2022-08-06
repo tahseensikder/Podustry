7e9f6a215224bb775e1a782a14b8dd65c1790c9b
tahseensikder <tahseensikder@gmail.com>
2022-08-06 10:18:21 +0600

    web_icon responsive

--- js/app.js
+++ js/app.js
@@ -119,4 +119,55 @@ $(function(){
     centerPadding: "0px",
     asNavFor:".testimonial_content",
   })
+  // web_icon
+  $(".web_icon_slider").slick({
+    slidesToShow:5,
+    slidesToScroll:1,
+    centerMode:true,
+    autoplay: true,
+    autoplaySpeed:3000,
+    centerPadding: "0px",
+    prevArrow:'.web_left',
+    nextArrow:'.web_right',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 768,
+           settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        }
+         // You can unslick at a given breakpoint now by adding:
+         // settings: "unslick"
+         // instead of a settings object
+    ]
+  })
 })
