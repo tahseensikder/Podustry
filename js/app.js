da119f786f2209da4db100e3d3ce4f2a431c7153
tahseensikder <tahseensikder@gmail.com>
2022-08-11 13:00:49 +0600

    responsive_portfolio_with slider

--- js/app.js
+++ js/app.js
@@ -103,7 +103,46 @@ $(function(){
   $('#num2').countMe(9,6);
   $('#num3').countMe(10,8);
   $('#num4').countMe(11,9);
-
+// portfolio
+   $('.port_slider').slick({
+    slidesToShow:4,
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+         settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+   ]
+  })
 // testimonial 
   $(".testimonial_content").slick({
     slidesToShow: 1,
