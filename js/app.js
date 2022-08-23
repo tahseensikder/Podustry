1d0c9df2db760bd9f8bca264f63d8042d520e137
tahseensikder <tahseensikder@gmail.com>
2022-08-23 12:41:16 +0600

    responsive about page

--- js/app.js
+++ js/app.js
@@ -247,8 +247,49 @@ $(function(){
     slidesToScroll:1,
     centerMode:true,
     centerPadding:'0px',
-    arrows:false,
-    autoplay:true,
+    // autoplay:true,
+    prevArrow:".team_left",
+    nextArrow:".team_right",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+         settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+  ]
   })
 })
 // About Us Page ends
